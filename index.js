3b99d058f32b52cc76b19eb03bf06dd0afbf43fe
bdbai <htbai1998m@hotmail.com>
2017-04-29 22:52:24 +0800

    Factor

--- index.js
+++ index.js
@@ -1,18 +1,2 @@
-#!/usr/bin/env node
-'use strict';
-
-if (require.main === module) {
-    const cli = require('./lib/cli');
-    const commands = require('commander')
-        .command('bootcdn-cli [<library[@version]>...]')
-        .description('Output library urls from bootcdn.cn.')
-        .option('-r, --raw ', 'don\'t wrap urls in HTML tags.')
-        .option('-a, --async ', 'add "async" to <script> tag.')
-        .option('-f, --force ', 'reset cache.')
-        .parse(process.argv);
-
-    cli(commands);
-} else {
-    module.exports = require('./lib/bootcdn');
-}
+module.exports = require('./lib/bootcdn');
 
